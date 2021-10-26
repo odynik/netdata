b33ff1f5367e02334b0a7102c21f5434b9718202
Vladimir Kobal <vlad@prokk.net>
2021-10-26 11:42:37 +0300

    Make the uptime chart collect a series

--- collectors/proc.plugin/proc_uptime.c
+++ collectors/proc.plugin/proc_uptime.c
@@ -33,10 +33,9 @@ int do_proc_uptime(int update_every, usec_t dt) {
                 , RRDSET_TYPE_LINE
         );
 
-        rrdset_flag_set(st, RRDSET_FLAG_STORE_FIRST);
+        // rrdset_flag_set(st, RRDSET_FLAG_STORE_FIRST);
 
         rd = rrddim_add(st, "uptime", NULL, 1, 1, RRD_ALGORITHM_ABSOLUTE);
-        sleep(5);
     }
     else
         rrdset_next(st);
