b545c02ce9bc33be065b8306bf93ad5be28a195c
odynik <odynik.ee@gmail.com>
2021-12-16 19:57:02 +0200

    [Replication] Implementation Plan

--- streaming/sender.c
+++ streaming/sender.c
@@ -718,6 +718,7 @@ void *rrdpush_sender_thread(void *ptr) {
                 sender_start(s);
                 buffer_sprintf(s->build, "TIMESTAMP %"PRId64"", (int64_t)now);
                 sender_commit(s);
+                // Send here the REP on command to the parent
             }
             rrdpush_claimed_id(s->host);
             continue;
