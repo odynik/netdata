5bdf493492f6aeab6d8dde46c0b362cc3b722cdf
odynik <odynik.ee@gmail.com>
2022-02-16 23:07:44 +0200

    [Replication + Libqueue] GAP evaluation - 1st attempt

--- libnetdata/queue/queue.h
+++ libnetdata/queue/queue.h
@@ -6,7 +6,23 @@
 #include "stdlib.h"
 #include "pthread.h"
 
+typedef struct node_s *node;
 typedef struct queue_s *queue_t;
+
+struct node_s{
+	void *item;
+	struct node_s *next;			
+};
+
+struct queue_s{		
+	pthread_cond_t conde;
+	pthread_cond_t condf;
+	pthread_mutex_t lock;
+	node front;			
+	node rear;
+	int max;
+	int count;
+};
 
 queue_t queue_new(int size);
 void queue_free(queue_t q);
